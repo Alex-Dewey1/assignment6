3bc61117f458c4e772f217a4fa96730a1a1e6ad3
Alexander Dewey <deweyal@flip2.engr.oregonstate.edu>
2025-05-30 16:17:00 -0700

    assignment 6 done

--- pq.c
+++ pq.c
@@ -4,29 +4,38 @@
  * you need in this file to implement a priority queue.  Make sure to add your
  * name and @oregonstate.edu email address below:
  *
- * Name:
- * Email:
+ * Name: Alexander Dewey
+ * Email: deweyal@oregonstate.edu
  */
 
 #include <stdlib.h>
-
+#include "dynarray.h"
 #include "pq.h"
-
+#include "assert.h"
+#include <stdio.h>
 /*
  * This is the structure that represents a priority queue.  You must define
  * this struct to contain the data needed to implement a priority queue.
  * in addition, you want to define an element struct with both data and priority, 
  * corresponding to the elements of the priority queue. 
  */
-struct pq;
+struct pq{
+	struct dynarray* heap;
+};
 
+struct pq_element{
+	int priority;
+	void* data;
+};
 
 /*
  * This function should allocate and initialize an empty priority queue and
  * return a pointer to it.
  */
 struct pq* pq_create() {
-  return NULL;
+	struct pq* new_pq = malloc(sizeof(struct pq));
+	new_pq->heap = dynarray_create();
+	return new_pq;
 }
 
 
@@ -39,7 +48,8 @@ struct pq* pq_create() {
  *   pq - the priority queue to be destroyed.  May not be NULL.
  */
 void pq_free(struct pq* pq) {
-
+	dynarray_free(pq->heap);
+	free(pq);
 }
 
 
@@ -55,7 +65,7 @@ void pq_free(struct pq* pq) {
  *   Should return 1 if pq is empty and 0 otherwise.
  */
 int pq_isempty(struct pq* pq) {
-  return 1;
+  return dynarray_length(pq->heap) == 0;
 }
 
 
@@ -77,7 +87,26 @@ int pq_isempty(struct pq* pq) {
  *     be the FIRST one returned.
  */
 void pq_insert(struct pq* pq, void* data, int priority) {
+	struct pq_element* new_element = malloc(sizeof(struct pq_element));
+	new_element->data = data;
+	new_element->priority = priority;
+	dynarray_insert(pq->heap, -1, new_element);
 
+	int current_index = dynarray_length(pq->heap) - 1;
+	while(current_index > 0){
+		int parent_index = (current_index - 1) / 2;
+		struct pq_element* parent_element = (struct pq_element*) dynarray_get(pq->heap, parent_index);
+		struct pq_element* current_element = (struct pq_element*) dynarray_get(pq->heap, current_index);
+
+		if(current_element->priority > parent_element->priority){
+			struct pq_element* temp = (struct pq_element*) dynarray_get(pq->heap, parent_index);
+			dynarray_set(pq->heap, parent_index, current_element);
+			dynarray_set(pq->heap, current_index, temp);
+			current_index = parent_index;
+		}else{
+			break;
+		}
+	}
 }
 
 
@@ -94,7 +123,11 @@ void pq_insert(struct pq* pq, void* data, int priority) {
  *   max priority value.
  */
 void* pq_max(struct pq* pq) {
-  return NULL;
+	if(dynarray_length(pq->heap) == 0){
+		return NULL;
+	}
+	struct pq_element* root_element = (struct pq_element*) dynarray_get(pq->heap, 0);
+	return root_element->data;
 }
 
 
@@ -111,7 +144,11 @@ void* pq_max(struct pq* pq) {
  *   with highest priority value.
  */
 int pq_max_priority(struct pq* pq) {
-  return 0;
+	if(dynarray_length(pq->heap) == 0){
+		return 0;
+	}
+	struct pq_element* root_element = (struct pq_element*) dynarray_get(pq->heap, 0);
+	return root_element->priority;
 }
 
 
@@ -129,5 +166,60 @@ int pq_max_priority(struct pq* pq) {
  *   highest priority value.
  */
 void* pq_max_dequeue(struct pq* pq) {
-  return NULL;
+    printf("%4d", dynarray_length(pq->heap));
+	if(dynarray_length(pq->heap) == 0){
+		return NULL;
+	}
+	struct pq_element* root_element = (struct pq_element*) dynarray_get(pq->heap, 0);
+	struct pq_element* last_element = (struct pq_element*) dynarray_get(pq->heap, -1);
+	dynarray_set(pq->heap, 0, last_element);
+	dynarray_remove(pq->heap, -1);
+
+	void* data_to_return = root_element->data;
+	free(root_element);
+
+	if(dynarray_length(pq->heap) == 1 || dynarray_length(pq->heap) == 0){
+		return data_to_return;
+	}
+
+	int current_index = 0;
+	while(1){
+		int left_index = 2 * current_index + 1;
+		int right_index = 2 * current_index + 2;
+		int largest_child_index = current_index;
+  		
+		assert(current_index < dynarray_length(pq->heap) || current_index == -1);
+		struct pq_element* current_element = (struct pq_element*) dynarray_get(pq->heap, current_index);
+
+		if(left_index < dynarray_length(pq->heap)){
+			assert(left_index < dynarray_length(pq->heap) || left_index == -1);
+			struct pq_element* left_element = (struct pq_element*) dynarray_get(pq->heap, left_index);
+			if(left_element->priority > current_element->priority){
+				largest_child_index = left_index;
+			}
+		}
+
+		if(right_index < dynarray_length(pq->heap)){
+			assert(right_index < dynarray_length(pq->heap) || right_index == -1);
+			struct pq_element* right_element = (struct pq_element*) dynarray_get(pq->heap, right_index);
+			assert(largest_child_index < dynarray_length(pq->heap) || largest_child_index == -1);
+			struct pq_element* largest_priority = (struct pq_element*) dynarray_get(pq->heap, largest_child_index);
+			if(right_element->priority > largest_priority->priority){
+				largest_child_index = right_index;
+			}
+		}
+
+		if(largest_child_index != current_index){
+			struct pq_element* temp = (struct pq_element*) dynarray_get(pq->heap, largest_child_index);
+			dynarray_set(pq->heap, largest_child_index, current_element);
+			dynarray_set(pq->heap, current_index, temp);
+			current_index = largest_child_index;
+		}else{
+			break;
+		}
+
+
+	}
+	return data_to_return;
+
 }
